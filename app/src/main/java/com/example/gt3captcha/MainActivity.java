c9582e49444a83e1a01670b1b65ce3f2089a3b2e
gunaonian <1069664125@qq.com>
2017-04-17 18:19:38 +0800

    执行之后成功，对状态的修改

--- app/src/main/java/com/example/gt3captcha/MainActivity.java
+++ app/src/main/java/com/example/gt3captcha/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 import com.example.sdk.GT3GeetestButton;
 import com.example.sdk.GT3GeetestUrl;
 import com.example.sdk.GT3GeetestUtils;
+import com.example.sdk.Gt3GeetestTestMsg;
+import com.example.sdk.Gt3GeetestViewPath;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
@@ -21,9 +23,9 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.ll_btn_type)
     GT3GeetestButton linearLayout;
     // 设置获取id，challenge，success的URL，需替换成自己的服务器URL
-    private static final String captchaURL = "http://www.geetest.com/demo/gt/register-slide";
+    private static final String captchaURL = "http://www.geetest.com/demo/gt/register-click";
     // 设置二次验证的URL，需替换成自己的服务器URL
-    private static final String validateURL = "http://www.geetest.com/demo/gt/validate-slide";
+    private static final String validateURL = "http://www.geetest.com/demo/gt/validate-click";
 
 
     @Override
@@ -49,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
     public void text(String a) {
         if (a.equals("dosuccess")) {
             //验证码验证成功后的操作
+           new Gt3GeetestTestMsg().setCandotouch(false);//执行这个方法表示验证成功后不能点击，不执行则表示可以点击
             Toast.makeText(getApplicationContext(), "这里是验证成功后执行的操作", Toast.LENGTH_SHORT).show();
         }
     }
